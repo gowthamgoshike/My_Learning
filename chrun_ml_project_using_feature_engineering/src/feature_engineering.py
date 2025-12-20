ece71c4701bf220b6b8676ce4a87caa22941cd9c
gowtham <gowthamgoshike23@gmail.com>
2025-12-20 09:09:16 -0500

    day 10 A Simple Feature Engineering project

--- chrun_ml_project_using_feature_engineering/src/feature_engineering.py
+++ chrun_ml_project_using_feature_engineering/src/feature_engineering.py
@@ -1,3 +1,5 @@
+# src/feature_engineering.py
+
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -7,10 +9,18 @@ class ChurnFeatureEngineer(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         X = X.copy()
-        X['avg_monthly_spend'] = X['TotalCharges'] / X['tenure'].replace(0, 1)
+        
+        # Convert numeric columns
+        X['TotalCharges'] = pd.to_numeric(X['TotalCharges'], errors='coerce')
+        X['tenure'] = pd.to_numeric(X['tenure'], errors='coerce').replace(0, 1)
+        X['MonthlyCharges'] = pd.to_numeric(X['MonthlyCharges'], errors='coerce')
+        
+        # Feature engineering
+        X['avg_monthly_spend'] = X['TotalCharges'] / X['tenure']
         X['tenure_bucket'] = pd.cut(
             X['tenure'], bins=[0,12,24,48,72],
             labels=['0-1yr','1-2yr','2-4yr','4-6yr']
         )
         X['high_monthly_charge'] = (X['MonthlyCharges'] > X['MonthlyCharges'].median()).astype(int)
+        
         return X
--- chrun_ml_project_using_feature_engineering/src/preprocessing.py
+++ chrun_ml_project_using_feature_engineering/src/preprocessing.py
@@ -1,12 +1,14 @@
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
-
-
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
 NUM_COLS = ['tenure', 'MonthlyCharges', 'TotalCharges', 'avg_monthly_spend']
 CAT_COLS = ['Contract', 'PaymentMethod', 'tenure_bucket']
 
-
 preprocessor = ColumnTransformer([
-('num', StandardScaler(), NUM_COLS),
-('cat', OneHotEncoder(handle_unknown='ignore'), CAT_COLS)
+    ('num', Pipeline([
+        ('imputer', SimpleImputer(strategy='median')),  # fill NaNs with median
+        ('scaler', StandardScaler())
+    ]), NUM_COLS),
+    ('cat', OneHotEncoder(handle_unknown='ignore'), CAT_COLS)
 ])
